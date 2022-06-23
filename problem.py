2d6aa6777ec479f136ad816bc98956eb9c438378
Md Sadiul Haque <sadiulhaquefahad56@gmail.com>
2022-06-24 00:26:34 +0600

    Update problem.py

--- problem.py
+++ problem.py
@@ -1,4 +1,5 @@
 import pandas as pd
+#import statsmodels.api as sm
 import matplotlib.pyplot as plt
 
 data = pd.read_csv('regression.csv')
@@ -10,9 +11,9 @@ plt.xlabel('SAT', fontsize = 20)
 plt.ylabel('GPA', fontsize = 20)
 plt.show()
 
-x = sm.add_constant(x1)
-results = sm.OLS(y,x).fit()
-results.summary()
+#x = sm.add_constant(x1)
+#results = sm.OLS(y,x).fit()
+#results.summary()
 plt.scatter(x1,y)
 yhat = 0.0017*x1 + 0.275
 fig = plt.plot(x1,yhat, lw=4, c='orange', label = 'regression line')

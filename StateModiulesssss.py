ba88c02124e15b3e625b3ebd9806a4027ef6cc69
Md Sadiul Haque <sadiulhaquefahad56@gmail.com>
2022-06-24 00:23:25 +0600

    update

--- StateModiulesssss.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-import matplotlib.pyplot as plt
-
-data = pd.read_csv('D:/Downlode File in Google/New folder/regression.csv')
-print(data.describe())
---
+++ problem.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+data = pd.read_csv('regression.csv')
+print(data.describe())
+y = data['GPA']
+x1 = data['SAT']
+plt.scatter(x1,y)
+plt.xlabel('SAT', fontsize = 20)
+plt.ylabel('GPA', fontsize = 20)
+plt.show()
+
+x = sm.add_constant(x1)
+results = sm.OLS(y,x).fit()
+results.summary()
+plt.scatter(x1,y)
+yhat = 0.0017*x1 + 0.275
+fig = plt.plot(x1,yhat, lw=4, c='orange', label = 'regression line')
+plt.xlabel('SAT', fontsize = 20)
+plt.ylabel('GPA', fontsize = 20)
+plt.show()
